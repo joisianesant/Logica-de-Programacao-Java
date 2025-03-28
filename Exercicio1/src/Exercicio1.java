0eb0b5e3fcb0a4252829256131fdfa7324dba91c
joisianesant <joisianess@gmail.com>
2025-03-28 12:18:09 -0400

    Meu primeiro commit

---
+++ Exercicio1/src/Exercicio1.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        int idade = 10;
+
+        System.out.println(idade);
+
+        }
+    }
