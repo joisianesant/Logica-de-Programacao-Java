edfbbbc7cdf29a4048007c9f0f0390a2a2f0e250
joisianesant <joisianess@gmail.com>
2025-03-28 14:03:19 -0400

    Fechando o exercicio 1 de raio

--- Exercicio1/src/Exercicio1.java
+++ Exercicio1/src/Exercicio1.java
@@ -1,15 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//  Faça um algoritmo para calcular a área de uma circunferência, considerando a
+//fórmula ÁREA = π * RAIO2. Utilize as variáveis AREA e RAIO, a constante π (pi =
+//3,14159) e os operadores aritméticos de multiplicação.
+
+
+import java.util.Scanner;
+
 public class Exercicio1 {
 
     public static void main(String[] args) {
 
-        int idade = 10;
-        int ano = 1998;
-        int dia = 2;
+        double pi = 3.14159;
+        double raio;
+        double area;
+        double resultadoraio;
 
-        System.out.println(idade);
-        System.out.println(ano);
-        System.out.println(dia);
+        System.out.println("Informe o valor do raio da sua circunferencia:");
+        raio = new Scanner(System.in).nextDouble();
+        resultadoraio = raio * raio;
+        area = pi * resultadoraio;
+        System.out.println("A área de sua circunferência:");
+        System.out.println(area);
+
         }
     }
