da810bea3b11f2e8600d9ed928bb34b8c7195587
joisianesant <joisianess@gmail.com>
2025-04-02 17:51:27 -0400

    Iniciando o exercicio 5

---
+++ Exercicio5/src/Exercicio5.java
@@ -0,0 +1,15 @@
+//Faça um algoritmo que:
+//a) Obtenha o valor para a variável HT (horas trabalhadas no mês);
+//b) Obtenha o valor para a variável VH (valor hora trabalhada):
+//c) Obtenha o valor para a variável PD (percentual de desconto);
+//d) Calcule o salário bruto => SB = HT * VH;
+//e) Calcule o total de desconto => TD = (PD/100)*SB;
+//f) Calcule o salário líquido => SL = SB – TD;
+//g) Apresente os valores de: Horas trabalhadas, Salário Bruto, Desconto, Salário
+//Liquido.
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+
+    }
+}
